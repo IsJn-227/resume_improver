4f228327454c05f055be14be921b9dda1b7b1f8d
IsJn-227 <ishjain2712@gmail.com>
2025-06-23 10:55:17 +0530

    Fix: Added try/except to spacy_filter.py to auto-download model

--- spacy_filter.py
+++ spacy_filter.py
@@ -1,4 +1,10 @@
 import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # Load spaCy's small English model
 nlp = spacy.load("en_core_web_sm")
